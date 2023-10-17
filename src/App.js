95e7da553813c81f600458d5429ad510a24513ad
Matheus Cardoso <matheus97_mcs@hotmail.com>
2023-10-17 19:47:46 -0300

    Criação do componente Time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#E8F8FF',
+      corSecundaria: '#82CFFA'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#F0F8E2',
+      corSecundaria: '#A6D157'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#FDE7E8',
+      corSecundaria: '#E06B69'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#FAE9F5',
+      corSecundaria: '#DB6EBF'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFF5D9',
+      corSecundaria: '#FFBA05'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FFEEDF',
+      corSecundaria: '#FF8A29'
+    }
+  ];
   const [colaboradores, setColaboradores] = useState([]);
 
   const adicionarColaborador = (colaborador) => {
@@ -15,6 +53,8 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => adicionarColaborador(colaborador)} />
+
+      {times.map(time => <Time key={time.nome} nome={time.nome} />)};
     </div>
   );
 }
